e9763dc653c364065562d859eda5905ca1c10a9b
Jonathan Braga <jonathan201392127@gmail.com>
2024-07-24 21:02:36 -0300

    save

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jonjon1992/pagamento-gateway
+
+go 1.22.5
---
+++ src/domain/entity/credit_card_test.go
@@ -0,0 +1,9 @@
+package entity
+
+import "testing"
+
+func TestCreditCardNumber(t *testing.T) {
+	creditCard, err := NewCreditCard("4324234234234234234234", "Jonathan Braga", 12, 2004, 121)
+
+	assert.Equal
+}
